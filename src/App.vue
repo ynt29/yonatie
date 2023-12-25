008d4dd20632b12e0725d587accb3b7ecf96b1f1
Yossakorn Napeang <ynt29tie@gmail.com>
2023-12-25 17:16:06 +0700

    Add Tie Heading

--- src/App.vue
+++ src/App.vue
@@ -11,6 +11,7 @@ import HelloWorld from './components/HelloWorld.vue'
       <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
     </a>
   </div>
+  <h1>Test Tie</h1>
   <HelloWorld msg="Vite + Vue" />
 </template>
 
